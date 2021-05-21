b9345484c6070f5ca451b427aee3fb4d5c163fde
AnastasijaVarchaciova <78699138+AnastasijaVarchaciova@users.noreply.github.com>
2021-05-21 22:14:23 +0100

    Add files via upload

---
+++ payment-notloggedin.php
@@ -0,0 +1,4 @@
+<!--not logged in heredoc - order summary and paypal button not shown if user is not logged in-->
+			<h3 class="text-start mt-5"><b>You need to have a BrainUp account to start a course</b></h3>
+			<br>
+			<button class="btn btn-primary mt-4" data-bs-target="#login_modal" data-bs-dismiss="modal" data-bs-toggle="modal">Login or Sign up</button>
